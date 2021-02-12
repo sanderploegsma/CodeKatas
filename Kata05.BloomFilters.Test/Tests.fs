c50ad4cd284dbe6f65b53083bd0d9529916064af
Sander Ploegsma <sanderploegsma@gmail.com>
2021-02-12 14:54:17 +0100

    Kata05: Bloom Filters

---
+++ Kata05.BloomFilters.Test/Tests.fs
@@ -0,0 +1,57 @@
+module Tests
+
+open BloomFilter
+open System
+open Xunit
+
+let rng = Random()
+
+let randomWord minLength maxLength =
+    let chars = Array.concat([[|'a' .. 'z'|];[|'A' .. 'Z'|];[|'0' .. '9'|]])
+    let sz = Array.length chars in
+    let length = rng.Next(minLength, maxLength)
+    String(Array.init length (fun _ -> chars.[rng.Next sz]))
+
+[<Fact>]
+let ``Empty BloomFilter contains no words`` () =
+    let filter = BloomFilter()
+    let word = randomWord 1 32
+    Assert.False(filter.contains word)
+    
+[<Fact>]
+let ``BloomFilter.contains returns true for initial words`` () =
+    let filter = BloomFilter()
+    
+    let dict = ["do"; "re"; "mi"; "fa"; "sol"; "la"; "ti"; "do"]
+    for word in dict do
+        filter.add word
+    
+    for word in dict do
+        Assert.True(filter.contains word)
+   
+[<Fact>]     
+let ``BloomFilter.contains returns false for other words`` () =
+    // Note: this test is a bit risky, because a Bloom Filter can always return false positives.
+    // However, we try to minimize this risk by initializing with longer words, and testing with
+    // shorter words.
+    
+    let filter = BloomFilter()
+    
+    let dict = Seq.init 100 (fun _ -> randomWord 8 32)
+    for word in dict do
+        filter.add word
+        
+    let words = List.except dict ["do"; "re"; "mi"; "fa"; "sol"; "la"; "ti"; "do"]
+    for word in words do
+        Assert.False(filter.contains word)
+        
+[<Fact>]
+let ``BloomFilter.contains returns false positive when hash function collides`` () =
+    // If we use a hash function that just returns the length of each word, then all words with the same length
+    // will yield the same hash, thus causing a collision.
+    
+    let filter = BloomFilter(hashFunctions = [String.length])
+    filter.add "foo"
+    
+    for word in Seq.init 100 (fun _ -> randomWord 3 3) do
+        Assert.True(filter.contains word)
---
+++ Kata05.BloomFilters/Program.fs
@@ -0,0 +1,23 @@
+﻿module Program
+
+open System
+open System.IO
+open BloomFilter
+
+let rec handleInput (filter: BloomFilter) =
+    printfn "Enter word to check, or type :q to quit:"
+    match Console.ReadLine() with
+    | ":q" -> 0
+    | word ->
+        printfn "Dictionary contains '%s': %b\n" word (filter.contains word)
+        handleInput filter
+        
+[<EntryPoint>]
+let main argv =
+    let filter = BloomFilter()
+    
+    printfn "Loading dictionary..."
+    for word in File.ReadLines("wordlist.txt") do
+        filter.add word
+    
+    handleInput filter
