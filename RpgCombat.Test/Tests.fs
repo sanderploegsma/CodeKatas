9dd01071a321a9d7587c12ecb4a8972d53fce20d
Sander Ploegsma <sanderploegsma@gmail.com>
2021-02-18 15:00:39 +0100

    RpgCombat iteration two

--- RpgCombat.Test/Tests.fs
+++ RpgCombat.Test/Tests.fs
@@ -5,66 +5,81 @@ open Xunit
 
 [<Fact>]
 let ``Character initial health is 1000`` () =
-    let sut = Character.create()
+    let sut = CombatEngine.createCharacter()
     Assert.Equal(1000, sut.Health)
     
 [<Fact>]
 let ``Character initial level is 1`` () =
-    let sut = Character.create()
+    let sut = CombatEngine.createCharacter()
     Assert.Equal(1, sut.Level)
     
 [<Fact>]
 let ``Character initial state is alive`` () =
-    let sut = Character.create()
+    let sut = CombatEngine.createCharacter()
     Assert.Equal(Alive, sut.State)
+
+[<Fact>]
+let ``Damaging a character only damages the target`` () =
+    let source = { Id = 1; Health = 1000; Level = 1; State = Alive }
+    let target = { Id = 2; Health = 1000; Level = 1; State = Alive }
+    let source', _ = CombatEngine.damage 100 source target        
+    Assert.Equal(source, source')
     
 [<Fact>]
 let ``Damaging a character subtracts the damage from their health`` () =
-    let sut =
-        Character.create()
-        |> Character.damage 100
-        
-    Assert.Equal(900, sut.Health)
+    let source = { Id = 1; Health = 1000; Level = 1; State = Alive }
+    let target = { Id = 2; Health = 1000; Level = 1; State = Alive }
+    let _, target' = CombatEngine.damage 100 source target        
+    Assert.Equal(900, target'.Health)
     
 [<Fact>]
 let ``Damaging a character for more than their health means the character dies`` () =
-    let sut =
-        Character.create()
-        |> Character.damage 1100
-        
-    Assert.Equal(Dead, sut.State)
+    let source = { Id = 1; Health = 1000; State = Alive; Level = 1 }
+    let target = { Id = 2; Health = 90; State = Alive; Level = 1 }
+    let _, target' = CombatEngine.damage 100 source target   
+    Assert.Equal(Dead, target'.State)
     
 [<Fact>]
 let ``Damaging a character for more than their health results in a health of 0`` () =
-    let sut =
-        Character.create()
-        |> Character.damage 1100
-        
-    Assert.Equal(0, sut.Health)
+    let source = { Id = 1; Health = 1000; State = Alive; Level = 1 }
+    let target = { Id = 2; Health = 90; State = Alive; Level = 1 }
+    let _, target' = CombatEngine.damage 100 source target   
+    Assert.Equal(0, target'.Health)
+    
+[<Fact>]
+let ``Damaging a character that is 5 or more levels lower increases damage by 50%`` () =
+    let source = { Id = 1; Health = 1000; Level = 6; State = Alive }
+    let target = { Id = 2; Health = 1000; Level = 1; State = Alive }
+    let _, target' = CombatEngine.damage 100 source target
+    Assert.Equal(850, target'.Health)
+    
+[<Fact>]
+let ``Damaging a character that is 5 or more levels higher reduces damage by 50%`` () =
+    let source = { Id = 1; Health = 1000; Level = 1; State = Alive }
+    let target = { Id = 2; Health = 1000; Level = 6; State = Alive }
+    let _, target' = CombatEngine.damage 100 source target
+    Assert.Equal(950, target'.Health)
+    
+[<Fact>]
+let ``Characters cannot damage themselves`` () =
+    let character = CombatEngine.createCharacter()
+    let source, target = CombatEngine.damage 100 character character
+    Assert.Equal(character, source)
+    Assert.Equal(character, target)
     
 [<Fact>]
 let ``Healing an alive character adds the healing points to their health`` () =
-    let sut =
-        Character.create()
-        |> Character.damage 500
-        |> Character.heal 100
-        
+    let sut = { Id = 1; Health = 500; Level = 1; State = Alive } |> CombatEngine.heal 100
     Assert.Equal(600, sut.Health)
     
 [<Fact>]
 let ``Healing an alive character never heals above 1000 health`` () =
-    let sut =
-        Character.create()
-        |> Character.heal 100
-        
+    let sut = { Id = 1; Health = 1000; Level = 1; State = Alive } |> CombatEngine.heal 100
     Assert.Equal(1000, sut.Health)
     
 [<Fact>]
 let ``Healing a dead character does nothing`` () =
-    let sut =
-        Character.create()
-        |> Character.damage 1000
-        |> Character.heal 100
+    let sut = { Id = 1; Health = 0; Level = 1; State = Dead } |> CombatEngine.heal 100
         
     Assert.Equal(Dead, sut.State)
     Assert.Equal(0, sut.Health)
--- RpgCombat/Library.fs
+++ RpgCombat/Library.fs
@@ -1,26 +1,47 @@
 ﻿module RpgCombat
 
-type CharacterState = Alive | Dead
+type CharacterState =
+    | Alive
+    | Dead
 
 type Character =
-    {
-        Health: int
-        Level: int
-        State: CharacterState
-    }
-    
+    { Id: int
+      Health: int
+      Level: int
+      State: CharacterState }
+
 [<RequireQualifiedAccess>]
-module Character =
-    let create () =
-        { Health = 1000; Level = 1; State = Alive }
-        
-    let damage points character =
-        if points < character.Health then
-            { character with Health = character.Health - points }
+module CombatEngine =
+    let mutable characters = 0
+
+    let createCharacter () =
+        characters <- characters + 1
+
+        { Id = characters
+          Health = 1000
+          Level = 1
+          State = Alive }
+
+    let damage (points: int) (source: Character) (target: Character) =
+        let multiplier =
+            if source.Level >= target.Level + 5 then 1.5
+            else if source.Level <= target.Level - 5 then 0.5
+            else 1.0
+
+        let trueDamage = (float points) * multiplier |> int // TODO: should health be a float?
+
+        if source.Id = target.Id then
+            source, target
+        else if trueDamage < target.Health then
+            source,
+            { target with
+                  Health = target.Health - trueDamage }
         else
-            { character with Health = 0; State = Dead }
-            
-    let heal points character =
-        match character.State with
-        | Dead -> character
-        | Alive -> { character with Health = min 1000 (character.Health + points) }
+            source, { target with Health = 0; State = Dead }
+
+    let heal (points: int) (target: Character) =
+        match target.State with
+        | Dead -> target
+        | Alive ->
+            { target with
+                  Health = min 1000 (target.Health + points) }
