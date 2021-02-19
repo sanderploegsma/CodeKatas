0ac03c9aec40872307277310020344cd4257718b
Sander Ploegsma <sanderploegsma@gmail.com>
2021-02-19 13:16:10 +0100

    RpgCombat iteration four

--- RpgCombat.Test/Tests.fs
+++ RpgCombat.Test/Tests.fs
@@ -3,6 +3,15 @@ namespace RpgCombat.Tests
 open RpgCombat
 open Xunit
 
+module TestHelpers =
+    let defaultCharacter =
+        { Health = 1000
+          Level = 1
+          State = Alive
+          FighterType = Melee
+          Position = 0, 0
+          Factions = Set.empty }
+
 module CharacterCreation =
     [<Fact>]
     let ``Character initial health is 1000`` () =
@@ -39,19 +48,29 @@ module CharacterCreation =
         match result with
         | Ok { Characters = characters } -> Assert.Equal(Alive, characters.[0].State)
         | Error s -> failwithf "Got error while creating character: %s" s
+        
+    [<Fact>]
+    let ``Character does not belong to any faction when created`` () =
+        let state = State.Init
+
+        let result =
+            CreateCharacter { FighterType = Melee }
+            |> run state
+
+        match result with
+        | Ok { Characters = characters } -> Assert.Empty(characters.[0].Factions)
+        | Error s -> failwithf "Got error while creating character: %s" s
 
 module Combat =
+    open TestHelpers
+
     [<Fact>]
     let ``Damaging a character subtracts the damage from their health`` () =
-        let character =
-            { Health = 1000
-              Level = 1
-              State = Alive
-              FighterType = Melee
-              Position = 0, 0 }
-
         let state =
-            { Characters = [ (0, character); (1, character) ] |> Map.ofList }
+            { Characters =
+                  [ (0, defaultCharacter)
+                    (1, defaultCharacter) ]
+                  |> Map.ofList }
 
         let result =
             DamageCharacter { Actor = 0; Target = 1; Damage = 500 }
@@ -63,15 +82,11 @@ module Combat =
 
     [<Fact>]
     let ``Damaging a character for more than their health means the character dies`` () =
-        let character =
-            { Health = 1000
-              Level = 1
-              State = Alive
-              FighterType = Melee
-              Position = 0, 0 }
-
         let state =
-            { Characters = [ (0, character); (1, character) ] |> Map.ofList }
+            { Characters =
+                  [ (0, defaultCharacter)
+                    (1, defaultCharacter) ]
+                  |> Map.ofList }
 
         let result =
             DamageCharacter { Actor = 0; Target = 1; Damage = 1100 }
@@ -85,14 +100,8 @@ module Combat =
 
     [<Fact>]
     let ``Damaging a character that is 5 or more levels lower increases damage by 50%`` () =
-        let source =
-            { Health = 1000
-              Level = 6
-              State = Alive
-              FighterType = Melee
-              Position = 0, 0 }
-
-        let target = { source with Level = 1 }
+        let source = { defaultCharacter with Level = 6 }
+        let target = { defaultCharacter with Level = 1 }
 
         let state =
             { Characters = [ (0, source); (1, target) ] |> Map.ofList }
@@ -107,14 +116,8 @@ module Combat =
 
     [<Fact>]
     let ``Damaging a character that is 5 or more levels higher reduces damage by 50%`` () =
-        let source =
-            { Health = 1000
-              Level = 1
-              State = Alive
-              FighterType = Melee
-              Position = 0, 0 }
-
-        let target = { source with Level = 6 }
+        let source = { defaultCharacter with Level = 1 }
+        let target = { defaultCharacter with Level = 6 }
 
         let state =
             { Characters = [ (0, source); (1, target) ] |> Map.ofList }
@@ -129,15 +132,8 @@ module Combat =
 
     [<Fact>]
     let ``Characters cannot damage themselves`` () =
-        let character =
-            { Health = 1000
-              Level = 1
-              State = Alive
-              FighterType = Melee
-              Position = 0, 0 }
-
         let state =
-            { Characters = [ (0, character) ] |> Map.ofList }
+            { Characters = [ (0, defaultCharacter) ] |> Map.ofList }
 
         let result =
             DamageCharacter { Actor = 0; Target = 0; Damage = 100 }
@@ -150,16 +146,11 @@ module Combat =
     [<Fact>]
     let ``Dead characters cannot damage anyone`` () =
         let source =
-            { Health = 0
-              Level = 1
-              State = Dead
-              FighterType = Melee
-              Position = 0, 0 }
+            { defaultCharacter with
+                  State = Dead
+                  Health = 0 }
 
-        let target =
-            { source with
-                  Health = 1000
-                  State = Alive }
+        let target = defaultCharacter
 
         let state =
             { Characters = [ (0, source); (1, target) ] |> Map.ofList }
@@ -174,14 +165,12 @@ module Combat =
 
     [<Fact>]
     let ``Characters cannot damage other dead characters`` () =
-        let source =
-            { Health = 1000
-              Level = 1
-              State = Alive
-              FighterType = Melee
-              Position = 0, 0 }
+        let source = defaultCharacter
 
-        let target = { source with Health = 0; State = Dead }
+        let target =
+            { defaultCharacter with
+                  Health = 0
+                  State = Dead }
 
         let state =
             { Characters = [ (0, source); (1, target) ] |> Map.ofList }
@@ -196,15 +185,8 @@ module Combat =
 
     [<Fact>]
     let ``Characters cannot damage non-existing characters`` () =
-        let source =
-            { Health = 1000
-              Level = 1
-              State = Alive
-              FighterType = Melee
-              Position = 0, 0 }
-
         let state =
-            { Characters = [ (0, source) ] |> Map.ofList }
+            { Characters = [ (0, defaultCharacter) ] |> Map.ofList }
 
         let result =
             DamageCharacter { Actor = 0; Target = 1; Damage = 100 }
@@ -217,13 +199,12 @@ module Combat =
     [<Fact>]
     let ``Melee fighters cannot attack someone who is more than 2 meters away from them`` () =
         let source =
-            { Health = 1000
-              Level = 1
-              State = Alive
-              FighterType = Melee
-              Position = 0, 0 }
+            { defaultCharacter with
+                  Position = 0, 0 }
 
-        let target = { source with Position = (2, 2) }
+        let target =
+            { defaultCharacter with
+                  Position = 2, 2 }
 
         let state =
             { Characters = [ (0, source); (1, target) ] |> Map.ofList }
@@ -239,13 +220,13 @@ module Combat =
     [<Fact>]
     let ``Ranged fighters can attack someone who is more than 2 meters away from them`` () =
         let source =
-            { Health = 1000
-              Level = 1
-              State = Alive
-              FighterType = Ranged
-              Position = 0, 0 }
+            { defaultCharacter with
+                  FighterType = Ranged
+                  Position = 0, 0 }
 
-        let target = { source with Position = (2, 2) }
+        let target =
+            { defaultCharacter with
+                  Position = 2, 2 }
 
         let state =
             { Characters = [ (0, source); (1, target) ] |> Map.ofList }
@@ -261,13 +242,13 @@ module Combat =
     [<Fact>]
     let ``Ranged fighters cannot attack someone who is more than 20 meters away from them`` () =
         let source =
-            { Health = 1000
-              Level = 1
-              State = Alive
-              FighterType = Melee
-              Position = 0, 0 }
+            { defaultCharacter with
+                  FighterType = Ranged
+                  Position = 0, 0 }
 
-        let target = { source with Position = (20, 10) }
+        let target =
+            { defaultCharacter with
+                  Position = 20, 10 }
 
         let state =
             { Characters = [ (0, source); (1, target) ] |> Map.ofList }
@@ -281,20 +262,17 @@ module Combat =
         | Error _ -> Assert.True(true) // Assert.Pass() anyone?
 
 module Healing =
+    open TestHelpers
+
     [<Fact>]
     let ``Healing an alive character adds the healing points to their health`` () =
-        let source =
-            { Health = 500
-              Level = 1
-              State = Alive
-              FighterType = Melee
-              Position = 0, 0 }
+        let character = { defaultCharacter with Health = 500 }
 
         let state =
-            { Characters = [ (0, source) ] |> Map.ofList }
+            { Characters = [ (0, character) ] |> Map.ofList }
 
         let result =
-            HealCharacter { Actor = 0; Points = 100 }
+            HealCharacter { Actor = 0; Target = 0; Health = 100 }
             |> run state
 
         match result with
@@ -303,18 +281,11 @@ module Healing =
 
     [<Fact>]
     let ``Healing an alive character never heals above 1000 health`` () =
-        let source =
-            { Health = 1000
-              Level = 1
-              State = Alive
-              FighterType = Melee
-              Position = 0, 0 }
-
         let state =
-            { Characters = [ (0, source) ] |> Map.ofList }
+            { Characters = [ (0, defaultCharacter) ] |> Map.ofList }
 
         let result =
-            HealCharacter { Actor = 0; Points = 100 }
+            HealCharacter { Actor = 0; Target = 0; Health = 100 }
             |> run state
 
         match result with
@@ -323,20 +294,88 @@ module Healing =
 
     [<Fact>]
     let ``Dead characters cannot heal themselves`` () =
-        let source =
-            { Health = 0
-              Level = 1
-              State = Dead
-              FighterType = Melee
-              Position = 0, 0 }
+        let character =
+            { defaultCharacter with
+                  State = Dead
+                  Health = 0 }
 
         let state =
-            { Characters = [ (0, source) ] |> Map.ofList }
+            { Characters = [ (0, character) ] |> Map.ofList }
 
         let result =
-            HealCharacter { Actor = 0; Points = 100 }
+            HealCharacter { Actor = 0; Target = 0; Health = 100 }
             |> run state
 
         match result with
         | Ok _ -> failwithf "Expected an error but got ok"
         | Error _ -> Assert.True(true) // Assert.Pass() anyone?
+
+    [<Fact>]
+    let ``Characters cannot heal other characters`` () =
+        let state =
+            { Characters =
+                  [ (0, defaultCharacter)
+                    (1, defaultCharacter) ]
+                  |> Map.ofList }
+
+        let result =
+            HealCharacter { Actor = 0; Target = 1; Health = 100 }
+            |> run state
+
+        match result with
+        | Ok _ -> failwithf "Expected an error but got ok"
+        | Error _ -> Assert.True(true) // Assert.Pass() anyone?
+
+module Factions =
+    open TestHelpers
+    
+    [<Fact>]
+    let ``Characters can join a faction`` () =
+        let state = { Characters = [(0, defaultCharacter)] |> Map.ofList }
+        let result = JoinFaction { Actor = 0; Faction = "Foo Fighters" } |> run state
+        
+        match result with
+        | Ok { Characters = characters } -> Assert.Contains("Foo Fighters", characters.[0].Factions)
+        | Error s -> failwithf "Got error while joining faction: %s" s
+        
+    [<Fact>]
+    let ``Characters can leave a faction`` () =
+        let character = { defaultCharacter with Factions = Set.ofList ["Foo Fighters"] }
+        let state = { Characters = [(0, character)] |> Map.ofList }
+        let result = LeaveFaction { Actor = 0; Faction = "Foo Fighters" } |> run state
+        
+        match result with
+        | Ok { Characters = characters } -> Assert.DoesNotContain("Foo Fighters", characters.[0].Factions)
+        | Error s -> failwithf "Got error while joining faction: %s" s
+        
+    [<Fact>]
+    let ``Characters belonging to the same faction cannot damage each other`` () =
+        let character1 = { defaultCharacter with Factions = Set.ofList ["Foo Fighters"; "Bar Fighters"] }
+        let character2 = { defaultCharacter with Factions = Set.ofList ["Foo Fighters"; "Baz Fighters"] }
+        let state = { Characters = [(0, character1); (1, character2)] |> Map.ofList }
+        
+        match DamageCharacter { Actor = 0; Target = 1; Damage = 10 } |> run state with
+        | Ok _ -> failwithf "Expected an error but got none"
+        | Error _ -> Assert.True(true)
+        
+        match DamageCharacter { Actor = 1; Target = 0; Damage = 10 } |> run state with
+        | Ok _ -> failwithf "Expected an error but got none"
+        | Error _ -> Assert.True(true)
+        
+    [<Fact>]
+    let ``Characters belonging to the same faction can heal each other`` () =
+        let character1 = { defaultCharacter with Health = 500; Factions = Set.ofList ["Foo Fighters"; "Bar Fighters"] }
+        let character2 = { defaultCharacter with Health = 400; Factions = Set.ofList ["Foo Fighters"; "Baz Fighters"] }
+        let state = { Characters = [(0, character1); (1, character2)] |> Map.ofList }
+        
+        match HealCharacter { Actor = 0; Target = 1; Health = 100 } |> run state with
+        | Ok { Characters = characters } ->
+            Assert.Equal(500, characters.[0].Health)
+            Assert.Equal(500, characters.[1].Health)
+        | Error s -> failwithf "Got error while healing character: %s" s
+        
+        match HealCharacter { Actor = 1; Target = 0; Health = 100 } |> run state with
+        | Ok { Characters = characters } ->
+            Assert.Equal(600, characters.[0].Health)
+            Assert.Equal(400, characters.[1].Health)
+        | Error s -> failwithf "Got error while healing character: %s" s
--- RpgCombat/Library.fs
+++ RpgCombat/Library.fs
@@ -15,7 +15,8 @@ type Character =
       Health: int
       Level: int
       State: CharacterState
-      Position: int * int }
+      Position: int * int
+      Factions: Set<string> }
 
 type CreateCharacterCommand = { FighterType: FighterType }
 
@@ -24,26 +25,44 @@ type DamageCharacterCommand =
       Target: CharacterId
       Damage: int }
 
-type HealCharacterCommand = { Actor: CharacterId; Points: int }
+type HealCharacterCommand =
+    { Actor: CharacterId
+      Target: CharacterId
+      Health: int }
+
+type JoinFactionCommand = { Actor: CharacterId; Faction: string }
+
+type LeaveFactionCommand = { Actor: CharacterId; Faction: string }
 
 type CharacterCreatedEvent = { FighterType: FighterType }
 
 type CharacterDamagedEvent =
     { Actor: CharacterId
       Target: CharacterId
-      Points: int }
+      Damage: int }
 
-type CharacterHealedEvent = { Actor: CharacterId; Points: int }
+type CharacterHealedEvent =
+    { Actor: CharacterId
+      Target: CharacterId
+      Health: int }
+
+type CharacterJoinedFactionEvent = { Actor: CharacterId; Faction: string }
+
+type CharacterLeftFactionEvent = { Actor: CharacterId; Faction: string }
 
 type Command =
     | CreateCharacter of CreateCharacterCommand
     | DamageCharacter of DamageCharacterCommand
     | HealCharacter of HealCharacterCommand
+    | JoinFaction of JoinFactionCommand
+    | LeaveFaction of LeaveFactionCommand
 
 type Event =
     | CharacterCreated of CharacterCreatedEvent
     | CharacterDamaged of CharacterDamagedEvent
     | CharacterHealed of CharacterHealedEvent
+    | CharacterJoinedFaction of CharacterJoinedFactionEvent
+    | CharacterLeftFaction of CharacterLeftFactionEvent
 
 type State =
     { Characters: Map<CharacterId, Character> }
@@ -74,6 +93,13 @@ let private execute state command =
         | Melee -> distance <= 2.0
         | Ranged -> distance <= 20.0
 
+    let areAllies sourceId targetId =
+        let { Factions = sourceFactions } = state.Characters.[sourceId]
+        let { Factions = targetFactions } = state.Characters.[targetId]
+
+        Set.intersect sourceFactions targetFactions
+        |> Set.count > 0
+
     match command with
     | CreateCharacter cmd -> Ok([ CharacterCreated { FighterType = cmd.FighterType } ])
 
@@ -83,28 +109,45 @@ let private execute state command =
     | DamageCharacter cmd when not (isAlive cmd.Target) -> Error "Cannot damage character who is already dead"
     | DamageCharacter cmd when cmd.Actor = cmd.Target -> Error "You cannot attack yourself"
     | DamageCharacter cmd when not (isInRange cmd.Actor cmd.Target) -> Error "Target is out of range"
+    | DamageCharacter cmd when areAllies cmd.Actor cmd.Target -> Error "Allies cannot damage each other"
     | DamageCharacter cmd ->
         Ok
             ([ CharacterDamaged
                 { Actor = cmd.Actor
                   Target = cmd.Target
-                  Points = cmd.Damage } ])
+                  Damage = cmd.Damage } ])
 
     | HealCharacter cmd when not (characterExists cmd.Actor) -> Error "Unknown character"
     | HealCharacter cmd when not (isAlive cmd.Actor) -> Error "Dead characters cannot heal"
+    | HealCharacter cmd when not (areAllies cmd.Actor cmd.Target)
+                             && cmd.Actor <> cmd.Target -> Error "Characters can only heal themselves or their allies"
     | HealCharacter cmd ->
         Ok
             ([ CharacterHealed
                 { Actor = cmd.Actor
-                  Points = cmd.Points } ])
+                  Target = cmd.Target
+                  Health = cmd.Health } ])
 
-let private applyDamage source target points =
+    | JoinFaction cmd when not (characterExists cmd.Actor) -> Error "Unknown character"
+    | JoinFaction cmd ->
+        Ok
+            ([ CharacterJoinedFaction
+                { Actor = cmd.Actor
+                  Faction = cmd.Faction } ])
+    | LeaveFaction cmd when not (characterExists cmd.Actor) -> Error "Unknown character"
+    | LeaveFaction cmd ->
+        Ok
+            ([ CharacterLeftFaction
+                { Actor = cmd.Actor
+                  Faction = cmd.Faction } ])
+
+let private applyDamage source target damage =
     let multiplier =
         if source.Level >= target.Level + 5 then 1.5
         else if source.Level <= target.Level - 5 then 0.5
         else 1.0
 
-    let trueDamage = (float points) * multiplier |> int // TODO: should health be a float?
+    let trueDamage = (float damage) * multiplier |> int // TODO: should health be a float?
 
 
     if trueDamage < target.Health then
@@ -113,8 +156,8 @@ let private applyDamage source target points =
     else
         { target with Health = 0; State = Dead }
 
-let private applyHeal target points =
-    let newHealth = min 1000 (target.Health + points)
+let private applyHeal (target: Character) health =
+    let newHealth = min 1000 (target.Health + health)
     { target with Health = newHealth }
 
 let private apply state event =
@@ -127,22 +170,43 @@ let private apply state event =
               Health = 1000
               Level = 1
               State = Alive
-              Position = 0, 0 }
+              Position = 0, 0
+              Factions = Set.empty }
 
         { state with
               Characters = Map.add id character state.Characters }
     | CharacterDamaged evt ->
         let character =
-            applyDamage state.Characters.[evt.Actor] state.Characters.[evt.Target] evt.Points
+            applyDamage state.Characters.[evt.Actor] state.Characters.[evt.Target] evt.Damage
 
         { state with
               Characters = Map.add evt.Target character state.Characters }
     | CharacterHealed evt ->
         let character =
-            applyHeal state.Characters.[evt.Actor] evt.Points
+            applyHeal state.Characters.[evt.Target] evt.Health
 
         { state with
-              Characters = Map.add evt.Actor character state.Characters }
+              Characters = Map.add evt.Target character state.Characters }
+
+    | CharacterJoinedFaction evt ->
+        let character = state.Characters.[evt.Actor]
+
+        let updatedCharacter =
+            { character with
+                  Factions = Set.add evt.Faction character.Factions }
+
+        { state with
+              Characters = Map.add evt.Actor updatedCharacter state.Characters }
+
+    | CharacterLeftFaction evt ->
+        let character = state.Characters.[evt.Actor]
+
+        let updatedCharacter =
+            { character with
+                  Factions = Set.remove evt.Faction character.Factions }
+
+        { state with
+              Characters = Map.add evt.Actor updatedCharacter state.Characters }
 
 let run state command =
     execute state command
