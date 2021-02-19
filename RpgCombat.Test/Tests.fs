dc52f7ed728f6453f8f907dd28c90286e34a724a
Sander Ploegsma <sanderploegsma@gmail.com>
2021-02-19 10:36:35 +0100

    RpgCombat iteration three
    Damn, big refactor

--- RpgCombat.Test/Tests.fs
+++ RpgCombat.Test/Tests.fs
@@ -1,85 +1,342 @@
-module RpgCombat.Tests
+namespace RpgCombat.Tests
 
 open RpgCombat
 open Xunit
 
-[<Fact>]
-let ``Character initial health is 1000`` () =
-    let sut = CombatEngine.createCharacter()
-    Assert.Equal(1000, sut.Health)
-    
-[<Fact>]
-let ``Character initial level is 1`` () =
-    let sut = CombatEngine.createCharacter()
-    Assert.Equal(1, sut.Level)
-    
-[<Fact>]
-let ``Character initial state is alive`` () =
-    let sut = CombatEngine.createCharacter()
-    Assert.Equal(Alive, sut.State)
+module CharacterCreation =
+    [<Fact>]
+    let ``Character initial health is 1000`` () =
+        let state = State.Init
 
-[<Fact>]
-let ``Damaging a character only damages the target`` () =
-    let source = { Id = 1; Health = 1000; Level = 1; State = Alive }
-    let target = { Id = 2; Health = 1000; Level = 1; State = Alive }
-    let source', _ = CombatEngine.damage 100 source target        
-    Assert.Equal(source, source')
-    
-[<Fact>]
-let ``Damaging a character subtracts the damage from their health`` () =
-    let source = { Id = 1; Health = 1000; Level = 1; State = Alive }
-    let target = { Id = 2; Health = 1000; Level = 1; State = Alive }
-    let _, target' = CombatEngine.damage 100 source target        
-    Assert.Equal(900, target'.Health)
-    
-[<Fact>]
-let ``Damaging a character for more than their health means the character dies`` () =
-    let source = { Id = 1; Health = 1000; State = Alive; Level = 1 }
-    let target = { Id = 2; Health = 90; State = Alive; Level = 1 }
-    let _, target' = CombatEngine.damage 100 source target   
-    Assert.Equal(Dead, target'.State)
-    
-[<Fact>]
-let ``Damaging a character for more than their health results in a health of 0`` () =
-    let source = { Id = 1; Health = 1000; State = Alive; Level = 1 }
-    let target = { Id = 2; Health = 90; State = Alive; Level = 1 }
-    let _, target' = CombatEngine.damage 100 source target   
-    Assert.Equal(0, target'.Health)
-    
-[<Fact>]
-let ``Damaging a character that is 5 or more levels lower increases damage by 50%`` () =
-    let source = { Id = 1; Health = 1000; Level = 6; State = Alive }
-    let target = { Id = 2; Health = 1000; Level = 1; State = Alive }
-    let _, target' = CombatEngine.damage 100 source target
-    Assert.Equal(850, target'.Health)
-    
-[<Fact>]
-let ``Damaging a character that is 5 or more levels higher reduces damage by 50%`` () =
-    let source = { Id = 1; Health = 1000; Level = 1; State = Alive }
-    let target = { Id = 2; Health = 1000; Level = 6; State = Alive }
-    let _, target' = CombatEngine.damage 100 source target
-    Assert.Equal(950, target'.Health)
-    
-[<Fact>]
-let ``Characters cannot damage themselves`` () =
-    let character = CombatEngine.createCharacter()
-    let source, target = CombatEngine.damage 100 character character
-    Assert.Equal(character, source)
-    Assert.Equal(character, target)
-    
-[<Fact>]
-let ``Healing an alive character adds the healing points to their health`` () =
-    let sut = { Id = 1; Health = 500; Level = 1; State = Alive } |> CombatEngine.heal 100
-    Assert.Equal(600, sut.Health)
-    
-[<Fact>]
-let ``Healing an alive character never heals above 1000 health`` () =
-    let sut = { Id = 1; Health = 1000; Level = 1; State = Alive } |> CombatEngine.heal 100
-    Assert.Equal(1000, sut.Health)
-    
-[<Fact>]
-let ``Healing a dead character does nothing`` () =
-    let sut = { Id = 1; Health = 0; Level = 1; State = Dead } |> CombatEngine.heal 100
-        
-    Assert.Equal(Dead, sut.State)
-    Assert.Equal(0, sut.Health)
+        let result =
+            CreateCharacter { FighterType = Melee }
+            |> run state
+
+        match result with
+        | Ok { Characters = characters } -> Assert.Equal(1000, characters.[0].Health)
+        | Error s -> failwithf "Got error while creating character: %s" s
+
+    [<Fact>]
+    let ``Character initial level is 1`` () =
+        let state = State.Init
+
+        let result =
+            CreateCharacter { FighterType = Melee }
+            |> run state
+
+        match result with
+        | Ok { Characters = characters } -> Assert.Equal(1, characters.[0].Level)
+        | Error s -> failwithf "Got error while creating character: %s" s
+
+    [<Fact>]
+    let ``Character initial state is alive`` () =
+        let state = State.Init
+
+        let result =
+            CreateCharacter { FighterType = Melee }
+            |> run state
+
+        match result with
+        | Ok { Characters = characters } -> Assert.Equal(Alive, characters.[0].State)
+        | Error s -> failwithf "Got error while creating character: %s" s
+
+module Combat =
+    [<Fact>]
+    let ``Damaging a character subtracts the damage from their health`` () =
+        let character =
+            { Health = 1000
+              Level = 1
+              State = Alive
+              FighterType = Melee
+              Position = 0, 0 }
+
+        let state =
+            { Characters = [ (0, character); (1, character) ] |> Map.ofList }
+
+        let result =
+            DamageCharacter { Actor = 0; Target = 1; Damage = 500 }
+            |> run state
+
+        match result with
+        | Ok { Characters = characters } -> Assert.Equal(500, characters.[1].Health)
+        | Error s -> failwithf "Got error while damaging character: %s" s
+
+    [<Fact>]
+    let ``Damaging a character for more than their health means the character dies`` () =
+        let character =
+            { Health = 1000
+              Level = 1
+              State = Alive
+              FighterType = Melee
+              Position = 0, 0 }
+
+        let state =
+            { Characters = [ (0, character); (1, character) ] |> Map.ofList }
+
+        let result =
+            DamageCharacter { Actor = 0; Target = 1; Damage = 1100 }
+            |> run state
+
+        match result with
+        | Ok { Characters = characters } ->
+            Assert.Equal(Dead, characters.[1].State)
+            Assert.Equal(0, characters.[1].Health)
+        | Error s -> failwithf "Got error while damaging character: %s" s
+
+    [<Fact>]
+    let ``Damaging a character that is 5 or more levels lower increases damage by 50%`` () =
+        let source =
+            { Health = 1000
+              Level = 6
+              State = Alive
+              FighterType = Melee
+              Position = 0, 0 }
+
+        let target = { source with Level = 1 }
+
+        let state =
+            { Characters = [ (0, source); (1, target) ] |> Map.ofList }
+
+        let result =
+            DamageCharacter { Actor = 0; Target = 1; Damage = 100 }
+            |> run state
+
+        match result with
+        | Ok { Characters = characters } -> Assert.Equal(850, characters.[1].Health)
+        | Error s -> failwithf "Got error while damaging character: %s" s
+
+    [<Fact>]
+    let ``Damaging a character that is 5 or more levels higher reduces damage by 50%`` () =
+        let source =
+            { Health = 1000
+              Level = 1
+              State = Alive
+              FighterType = Melee
+              Position = 0, 0 }
+
+        let target = { source with Level = 6 }
+
+        let state =
+            { Characters = [ (0, source); (1, target) ] |> Map.ofList }
+
+        let result =
+            DamageCharacter { Actor = 0; Target = 1; Damage = 100 }
+            |> run state
+
+        match result with
+        | Ok { Characters = characters } -> Assert.Equal(950, characters.[1].Health)
+        | Error s -> failwithf "Got error while damaging character: %s" s
+
+    [<Fact>]
+    let ``Characters cannot damage themselves`` () =
+        let character =
+            { Health = 1000
+              Level = 1
+              State = Alive
+              FighterType = Melee
+              Position = 0, 0 }
+
+        let state =
+            { Characters = [ (0, character) ] |> Map.ofList }
+
+        let result =
+            DamageCharacter { Actor = 0; Target = 0; Damage = 100 }
+            |> run state
+
+        match result with
+        | Ok _ -> failwithf "Expected an error but got ok"
+        | Error _ -> Assert.True(true) // Assert.Pass() anyone?
+
+    [<Fact>]
+    let ``Dead characters cannot damage anyone`` () =
+        let source =
+            { Health = 0
+              Level = 1
+              State = Dead
+              FighterType = Melee
+              Position = 0, 0 }
+
+        let target =
+            { source with
+                  Health = 1000
+                  State = Alive }
+
+        let state =
+            { Characters = [ (0, source); (1, target) ] |> Map.ofList }
+
+        let result =
+            DamageCharacter { Actor = 0; Target = 1; Damage = 100 }
+            |> run state
+
+        match result with
+        | Ok _ -> failwithf "Expected an error but got ok"
+        | Error _ -> Assert.True(true) // Assert.Pass() anyone?
+
+    [<Fact>]
+    let ``Characters cannot damage other dead characters`` () =
+        let source =
+            { Health = 1000
+              Level = 1
+              State = Alive
+              FighterType = Melee
+              Position = 0, 0 }
+
+        let target = { source with Health = 0; State = Dead }
+
+        let state =
+            { Characters = [ (0, source); (1, target) ] |> Map.ofList }
+
+        let result =
+            DamageCharacter { Actor = 0; Target = 1; Damage = 100 }
+            |> run state
+
+        match result with
+        | Ok _ -> failwithf "Expected an error but got ok"
+        | Error _ -> Assert.True(true) // Assert.Pass() anyone?
+
+    [<Fact>]
+    let ``Characters cannot damage non-existing characters`` () =
+        let source =
+            { Health = 1000
+              Level = 1
+              State = Alive
+              FighterType = Melee
+              Position = 0, 0 }
+
+        let state =
+            { Characters = [ (0, source) ] |> Map.ofList }
+
+        let result =
+            DamageCharacter { Actor = 0; Target = 1; Damage = 100 }
+            |> run state
+
+        match result with
+        | Ok _ -> failwithf "Expected an error but got ok"
+        | Error _ -> Assert.True(true) // Assert.Pass() anyone?
+
+    [<Fact>]
+    let ``Melee fighters cannot attack someone who is more than 2 meters away from them`` () =
+        let source =
+            { Health = 1000
+              Level = 1
+              State = Alive
+              FighterType = Melee
+              Position = 0, 0 }
+
+        let target = { source with Position = (2, 2) }
+
+        let state =
+            { Characters = [ (0, source); (1, target) ] |> Map.ofList }
+
+        let result =
+            DamageCharacter { Actor = 0; Target = 1; Damage = 100 }
+            |> run state
+
+        match result with
+        | Ok _ -> failwithf "Expected an error but got ok"
+        | Error _ -> Assert.True(true) // Assert.Pass() anyone?
+
+    [<Fact>]
+    let ``Ranged fighters can attack someone who is more than 2 meters away from them`` () =
+        let source =
+            { Health = 1000
+              Level = 1
+              State = Alive
+              FighterType = Ranged
+              Position = 0, 0 }
+
+        let target = { source with Position = (2, 2) }
+
+        let state =
+            { Characters = [ (0, source); (1, target) ] |> Map.ofList }
+
+        let result =
+            DamageCharacter { Actor = 0; Target = 1; Damage = 100 }
+            |> run state
+
+        match result with
+        | Ok _ -> Assert.True(true) // Assert.Pass() anyone?
+        | Error s -> failwithf "Got error while damaging character: %s" s
+
+    [<Fact>]
+    let ``Ranged fighters cannot attack someone who is more than 20 meters away from them`` () =
+        let source =
+            { Health = 1000
+              Level = 1
+              State = Alive
+              FighterType = Melee
+              Position = 0, 0 }
+
+        let target = { source with Position = (20, 10) }
+
+        let state =
+            { Characters = [ (0, source); (1, target) ] |> Map.ofList }
+
+        let result =
+            DamageCharacter { Actor = 0; Target = 1; Damage = 100 }
+            |> run state
+
+        match result with
+        | Ok _ -> failwithf "Expected an error but got ok"
+        | Error _ -> Assert.True(true) // Assert.Pass() anyone?
+
+module Healing =
+    [<Fact>]
+    let ``Healing an alive character adds the healing points to their health`` () =
+        let source =
+            { Health = 500
+              Level = 1
+              State = Alive
+              FighterType = Melee
+              Position = 0, 0 }
+
+        let state =
+            { Characters = [ (0, source) ] |> Map.ofList }
+
+        let result =
+            HealCharacter { Actor = 0; Points = 100 }
+            |> run state
+
+        match result with
+        | Ok { Characters = characters } -> Assert.Equal(600, characters.[0].Health)
+        | Error s -> failwithf "Got error while healing character: %s" s
+
+    [<Fact>]
+    let ``Healing an alive character never heals above 1000 health`` () =
+        let source =
+            { Health = 1000
+              Level = 1
+              State = Alive
+              FighterType = Melee
+              Position = 0, 0 }
+
+        let state =
+            { Characters = [ (0, source) ] |> Map.ofList }
+
+        let result =
+            HealCharacter { Actor = 0; Points = 100 }
+            |> run state
+
+        match result with
+        | Ok { Characters = characters } -> Assert.Equal(1000, characters.[0].Health)
+        | Error s -> failwithf "Got error while healing character: %s" s
+
+    [<Fact>]
+    let ``Dead characters cannot heal themselves`` () =
+        let source =
+            { Health = 0
+              Level = 1
+              State = Dead
+              FighterType = Melee
+              Position = 0, 0 }
+
+        let state =
+            { Characters = [ (0, source) ] |> Map.ofList }
+
+        let result =
+            HealCharacter { Actor = 0; Points = 100 }
+            |> run state
+
+        match result with
+        | Ok _ -> failwithf "Expected an error but got ok"
+        | Error _ -> Assert.True(true) // Assert.Pass() anyone?
--- RpgCombat/Library.fs
+++ RpgCombat/Library.fs
@@ -1,47 +1,149 @@
 ﻿module RpgCombat
 
+type CharacterId = int
+
 type CharacterState =
     | Alive
     | Dead
 
+type FighterType =
+    | Melee
+    | Ranged
+
 type Character =
-    { Id: int
+    { FighterType: FighterType
       Health: int
       Level: int
-      State: CharacterState }
+      State: CharacterState
+      Position: int * int }
 
-[<RequireQualifiedAccess>]
-module CombatEngine =
-    let mutable characters = 0
+type CreateCharacterCommand = { FighterType: FighterType }
 
-    let createCharacter () =
-        characters <- characters + 1
+type DamageCharacterCommand =
+    { Actor: CharacterId
+      Target: CharacterId
+      Damage: int }
 
-        { Id = characters
-          Health = 1000
-          Level = 1
-          State = Alive }
+type HealCharacterCommand = { Actor: CharacterId; Points: int }
 
-    let damage (points: int) (source: Character) (target: Character) =
-        let multiplier =
-            if source.Level >= target.Level + 5 then 1.5
-            else if source.Level <= target.Level - 5 then 0.5
-            else 1.0
+type CharacterCreatedEvent = { FighterType: FighterType }
 
-        let trueDamage = (float points) * multiplier |> int // TODO: should health be a float?
+type CharacterDamagedEvent =
+    { Actor: CharacterId
+      Target: CharacterId
+      Points: int }
 
-        if source.Id = target.Id then
-            source, target
-        else if trueDamage < target.Health then
-            source,
-            { target with
-                  Health = target.Health - trueDamage }
-        else
-            source, { target with Health = 0; State = Dead }
+type CharacterHealedEvent = { Actor: CharacterId; Points: int }
 
-    let heal (points: int) (target: Character) =
-        match target.State with
-        | Dead -> target
-        | Alive ->
-            { target with
-                  Health = min 1000 (target.Health + points) }
+type Command =
+    | CreateCharacter of CreateCharacterCommand
+    | DamageCharacter of DamageCharacterCommand
+    | HealCharacter of HealCharacterCommand
+
+type Event =
+    | CharacterCreated of CharacterCreatedEvent
+    | CharacterDamaged of CharacterDamagedEvent
+    | CharacterHealed of CharacterHealedEvent
+
+type State =
+    { Characters: Map<CharacterId, Character> }
+    static member Init = { Characters = Map.empty }
+
+let private execute state command =
+    let characterExists characterId =
+        state.Characters.ContainsKey(characterId)
+
+    let isAlive characterId =
+        state.Characters.TryFind(characterId)
+        |> Option.map (fun c -> c.State = Alive)
+        |> Option.defaultValue false
+
+    let isInRange sourceId targetId =
+        let { FighterType = fighterType
+              Position = (x1, y1) } =
+            state.Characters.[sourceId]
+
+        let { Position = (x2, y2) } = state.Characters.[targetId]
+
+        let distance =
+            pown (x2 - x1) 2 + pown (y2 - y1) 2
+            |> float
+            |> sqrt
+
+        match fighterType with
+        | Melee -> distance <= 2.0
+        | Ranged -> distance <= 20.0
+
+    match command with
+    | CreateCharacter cmd -> Ok([ CharacterCreated { FighterType = cmd.FighterType } ])
+
+    | DamageCharacter cmd when not (characterExists cmd.Actor) -> Error "Unknown character"
+    | DamageCharacter cmd when not (characterExists cmd.Target) -> Error "Unknown character"
+    | DamageCharacter cmd when not (isAlive cmd.Actor) -> Error "Dead characters cannot damage other characters"
+    | DamageCharacter cmd when not (isAlive cmd.Target) -> Error "Cannot damage character who is already dead"
+    | DamageCharacter cmd when cmd.Actor = cmd.Target -> Error "You cannot attack yourself"
+    | DamageCharacter cmd when not (isInRange cmd.Actor cmd.Target) -> Error "Target is out of range"
+    | DamageCharacter cmd ->
+        Ok
+            ([ CharacterDamaged
+                { Actor = cmd.Actor
+                  Target = cmd.Target
+                  Points = cmd.Damage } ])
+
+    | HealCharacter cmd when not (characterExists cmd.Actor) -> Error "Unknown character"
+    | HealCharacter cmd when not (isAlive cmd.Actor) -> Error "Dead characters cannot heal"
+    | HealCharacter cmd ->
+        Ok
+            ([ CharacterHealed
+                { Actor = cmd.Actor
+                  Points = cmd.Points } ])
+
+let private applyDamage source target points =
+    let multiplier =
+        if source.Level >= target.Level + 5 then 1.5
+        else if source.Level <= target.Level - 5 then 0.5
+        else 1.0
+
+    let trueDamage = (float points) * multiplier |> int // TODO: should health be a float?
+
+
+    if trueDamage < target.Health then
+        { target with
+              Health = target.Health - trueDamage }
+    else
+        { target with Health = 0; State = Dead }
+
+let private applyHeal target points =
+    let newHealth = min 1000 (target.Health + points)
+    { target with Health = newHealth }
+
+let private apply state event =
+    match event with
+    | CharacterCreated evt ->
+        let id = Map.count state.Characters
+
+        let character =
+            { FighterType = evt.FighterType
+              Health = 1000
+              Level = 1
+              State = Alive
+              Position = 0, 0 }
+
+        { state with
+              Characters = Map.add id character state.Characters }
+    | CharacterDamaged evt ->
+        let character =
+            applyDamage state.Characters.[evt.Actor] state.Characters.[evt.Target] evt.Points
+
+        { state with
+              Characters = Map.add evt.Target character state.Characters }
+    | CharacterHealed evt ->
+        let character =
+            applyHeal state.Characters.[evt.Actor] evt.Points
+
+        { state with
+              Characters = Map.add evt.Actor character state.Characters }
+
+let run state command =
+    execute state command
+    |> Result.map (List.fold apply state)
